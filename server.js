a68f2ac5711a6fc2ef6337a5f5464328356e6bd7
sevrinbe <sevrin.eldred@gmail.com>
2022-08-28 14:36:13 -0500

    added delete function, completed post functionality, have click event showing note text and title when li is clicked

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const notesDB = require('./db/db.json');
+const fs = require("fs");
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,6 +23,60 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => 
+  res.json(notesDB)
+);
+
+app.post('/api/notes', (req, res) => {
+  const {title, text} = req.body;
+  const newNote = {
+    title,
+    text,
+    id: uuid(),
+  }
+  notesDB.push(newNote);
+  res.json(notesDB);
+  console.log(notesDB);
+
+
+  fs.writeFile(`./db/db.json`, JSON.stringify(notesDB, null, 4), (err) =>
+  err
+    ? console.error(err)
+    : console.log(
+        `Note has been written to JSON file`
+      )
+  );
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const clickedID  = req.params.id;
+  console.log(clickedID)
+
+  res.json(notesDB);
+  
+  let noteToDelete;
+  for(i = 0; i < notesDB.length; i++) {
+    let id = notesDB[i].id;
+    if (id == clickedID) {
+      console.log("hit")
+      noteToDelete = i;
+      break;
+    }
+  }
+
+  console.log(noteToDelete);
+  notesDB.splice(noteToDelete, 1);
+  console.log(notesDB)
+  fs.writeFile(`./db/db.json`, JSON.stringify(notesDB, null, 4), (err) =>
+  err
+    ? console.error(err)
+    : console.log(
+        `Note has been DELETED from JSON file`
+      )
+  );
+});
+
 
 
 app.listen(PORT, () =>
